b8763ec5952c6c6aac093e72f58afdeb6f52f956
Donald Huber-Youmans <donaldhuberyoumans@gmail.com>
2025-04-22 08:54:57 +0200

    Restructuring

--- plugin/focus-cmd-maps.lua
+++ plugin/focus-cmd-maps.lua
@@ -1,7 +1,7 @@
 vim.keymap.set('n', '::', function()
-  require('focus-cmd').open_cmd_window('::')
+  require('focus-cmd').focus_normal_mode()
 end)
 
 vim.keymap.set('n', '/', function()
-  require('focus-cmd').open_cmd_window('/')
+  require('focus-cmd').focus_search()
 end)
