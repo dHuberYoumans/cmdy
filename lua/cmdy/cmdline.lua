febba3dc3c597bad2eb006b2748a88cf2561877f
Donald Huber-Youmans <donaldhuberyoumans@gmail.com>
2025-05-29 21:16:03 +0200

    refactor(cmdline): add colored prompt symbol

--- lua/cmdy/cmdline.lua
+++ lua/cmdy/cmdline.lua
@@ -16,16 +16,17 @@ local src = {
 local M = {fn = {}}
 local fn = {}
 
+
 function M.mirror_cmdline()
     state.buf = vim.api.nvim_create_buf(false,true)
     state.mirror, border = window.create_window(state.buf, config.prompt_defaults)
-
     hl.setup_highlights()
     window.apply_highlights(state.mirror, border)
-
     local line = vim.fn.getcmdline()
-    vim.api.nvim_buf_set_lines(state.buf, 0, -1, false, {":" .. line})
-    
+    vim.api.nvim_buf_set_lines(state.buf, 0, -1, false, {"❯ " .. line})
+    vim.api.nvim_buf_call(state.buf, function()
+        vim.fn.matchadd("FocusCmdPromptChar", [[\v^(❯|\/)]])
+    end)
     vim.cmd('redraw')
 end
 
@@ -37,7 +38,12 @@ vim.api.nvim_create_autocmd("BufEnter", {
 
 function fn.update_cmdline_content()
     local line = vim.fn.getcmdline()
-    if state.buf then vim.api.nvim_buf_set_lines(state.buf,0,-1,false, {":"..line}) end
+    if state.buf then
+        vim.api.nvim_buf_set_lines(state.buf,0,-1,false, {"❯ "..line}) 
+        vim.api.nvim_buf_call(state.buf, function()
+            vim.fn.matchadd("FocusCmdPromptChar", [[\v^(❯|\/)]])
+        end)
+    end
     vim.cmd('redraw')
 end
 
