c33b18cc66167c9849c1746092e8b1e9ae5407db
Donald Huber-Youmans <donaldhuberyoumans@gmail.com>
2025-09-27 19:29:08 +0200

    fix: bug causing the cmdline mirroring to error
    We attempt to fix a bug where the mirroring of the cmdline would error.
    
    The following command reproduces the error reliably:
    
    1. search using vimgrep for anything that can be found in your project
    
    2. repeat
    
    On the second `/` of $ vimgrep /pattern/ in the second step the plugin
    errors complaining about an `Invalid lnum`. The bug is likely caused due
    to an expansion of the search history silently invoked by vimgrep. This
    search history may consist of a long buffer leading to the `Invalid
    lnum` error.
    
    After some iterations we have not yet found a clean fix for this.
    
    What we have tried so far:
    1. clean state.buf and state.mirror
    2. defer/debounce the input
    3. use `vim.api.nvim_buf_set_text` instead of
       `vim.api.nvim_buf_set_lines`
    
    Eventually, we call `update_cmdline` inside a pcall and skip the update
    on error. You can see this by using :messages (at debug level).

--- lua/cmdy/cmdline.lua
+++ lua/cmdy/cmdline.lua
@@ -3,7 +3,7 @@ local config = require('cmdy.config')
 local hl = require('cmdy.highlight')
 
 local state = {
-    mirror = nil, 
+    mirror = nil,
     mirror_opts = nil,
     buf = nil,
 }
@@ -18,8 +18,23 @@ local prompt = config.options.cmdline.prompt
 local M = {fn = {}}
 local fn = {}
 
+local function clean_states()
+    if state.mirror and vim.api.nvim_win_is_valid(state.mirror) then
+        pcall(vim.api.nvim_win_close, state.mirror, true)
+    end
+    if state.buf and vim.api.nvim_buf_is_valid(state.buf) then
+        pcall(vim.api.nvim_buf_delete, state.buf, { force = true })
+    end
+    state.buf = nil
+    state.mirror = nil
+end
+
 function M.mirror_cmdline()
+    local border
     state.buf = vim.api.nvim_create_buf(false,true)
+    vim.api.nvim_set_option_value("buftype", "nofile", { buf = state.buf})
+    vim.api.nvim_set_option_value("bufhidden", "hide", { buf = state.buf})
+    vim.api.nvim_set_option_value("swapfile", false, { buf = state.buf} )
     state.mirror, border = window.create_window(state.buf, config.options.prompt)
     hl.setup_highlights()
     window.apply_highlights(state.mirror, border)
@@ -38,23 +53,38 @@ vim.api.nvim_create_autocmd("BufEnter", {
 })
 
 function fn.update_cmdline_content()
-    local line = vim.fn.getcmdline()
-    if state.buf then
-        vim.api.nvim_buf_set_lines(state.buf,0,-1,false, {prompt .. " " .. line}) 
-        vim.api.nvim_buf_call(state.buf, function()
-            hl.hl_prompt(prompt)
-        end)
+    if not (state.mirror and vim.api.nvim_win_is_valid(state.mirror)) then
+        return
     end
-    vim.cmd('redraw')
+    local line = vim.fn.getcmdline()
+    if state.buf and vim.api.nvim_buf_is_valid(state.buf) then
+        local ok, err = pcall(function ()
+            vim.api.nvim_buf_set_lines(
+                state.buf,
+                0,
+                1,
+                false,
+                {prompt .. " " .. line}
+            )
+            vim.api.nvim_buf_call(state.buf, function()
+                hl.hl_prompt(prompt)
+            end)
+        end)
+        if not ok then
+            vim.notify("update skipped: " .. err, vim.log.levels.DEBUG)
+        end
+        vim.cmd('redraw')
+    end
 end
 
 vim.api.nvim_create_autocmd("CmdlineEnter", {
     callback = function()
-        src.win = vim.api.nvim_get_current_win()
-        src.buf = vim.api.nvim_get_current_buf()
-        vim.schedule(function()
-            M.mirror_cmdline()
-        end)
+            src.win = vim.api.nvim_get_current_win()
+            src.buf = vim.api.nvim_get_current_buf()
+            clean_states()
+            vim.schedule(function()
+                M.mirror_cmdline()
+            end)
     end,
 })
 
@@ -71,9 +101,7 @@ vim.api.nvim_create_autocmd("CmdlineLeave", {
 	vim.api.nvim_set_current_win(src.win)
 	vim.api.nvim_set_current_buf(src.buf)
         vim.schedule(function()
-            if vim.api.nvim_win_is_valid(state.mirror) then
-                vim.api.nvim_win_close(state.mirror, true)
-            end
+            clean_states()
         end)
     end,
 })
